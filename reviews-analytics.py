50ae75f78d90bba8f5fbe6d0b86c8096878c2fff
Wayne Lin <wayne23726489@gmail.com>
2020-02-01 17:26:32 +0800

    可計算留言筆數、留言平均字數

---
+++ reviews-analytics.py
@@ -0,0 +1,18 @@
+data = []
+count = 0
+read = 0
+len_data = 0
+with open("reviews.txt", "r") as a:
+	for line in a:
+		data.append(line)
+
+print("檔案讀取完畢，本檔案共有",len(data),"筆留言")
+
+for cm in data:
+	len_data = len_data + len(cm)
+print("平均字數為",len_data/len(data))
+
+#while read > -5:
+#	print("This is comment no.",1000000+read)
+#	print(data[read])
+#	read -= 1
